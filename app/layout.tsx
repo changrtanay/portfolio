de7cea452549e386812b7ad338d668486ba4db73
Tanay Changoiwala <72229839+changrtanay@users.noreply.github.com>
2026-01-28 22:16:55 +0530

    Update favicon configuration in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,10 +31,10 @@ export const metadata: Metadata = {
   metadataBase: new URL("https://tanaychangoiwala.vercel.app"),
   icons: {
     icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon.ico" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
     ],
-    icon: "/favicon.ico",
     apple: "/apple-touch-icon.png",
   },
   manifest: "/site.webmanifest",
