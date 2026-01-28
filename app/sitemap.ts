1f6c8d7189f2e4b3b59e70bd5615ff354479fcd4
Tanay Changoiwala <72229839+changrtanay@users.noreply.github.com>
2026-01-28 19:49:19 +0530

    Remove changeFrequency and priority from sitemap
    Removed changeFrequency and priority from sitemap entries.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,8 +5,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://tanaychangoiwala.vercel.app",
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
     },
   ];
 }
