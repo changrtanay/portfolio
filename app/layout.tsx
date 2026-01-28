d6dcc650ea513e70af3f6c07479d4c50e61e34f1
Tanay Changoiwala <72229839+changrtanay@users.noreply.github.com>
2026-01-28 19:41:19 +0530

    Refactor metadata for improved SEO and schema
    Updated metadata structure and added additional properties for SEO and schema.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,33 @@ import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
+import type { Metadata } from "next";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Tanay | Personal Portfolio",
-  description: "Tanay Changoiwala",
-  verification: {
-    google: "Jf-ibs0v5f0NsTc1jm3eS8cKRrHWNsy6ZStjs-RXncI",
+export const metadata: Metadata = {
+  title: {
+    default: "Tanay Changoiwala",
   },
-  robots: {
-    index: true,
-    follow: true,
+  description:
+    "Hello, I'm Tanay Changoiwala. I'm a CSE Student. I like building impactful applications that solve real-world problems.",
+  keywords: [
+    "Tanay Changoiwala",
+    "Tanay Changoiwala Portfolio",
+    "CSE Student",
+    "Next.js Developer",
+    "Competitive Programming",
+  ],
+  metadataBase: new URL("https://tanaychangoiwala.vercel.app"),
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
   },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
@@ -31,6 +45,25 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
+        <Script
+          id="schema-person"
+          type="application/ld+json"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Person",
+              name: "Tanay Changoiwala",
+              url: "https://tanaychangoiwala.vercel.app",
+              sameAs: [
+                "https://www.linkedin.com/in/tanaychangoiwala/",
+                "https://github.com/changrtanay",
+                "https://leetcode.com/u/changrtanay/",
+                "https://codeforces.com/profile/tanay092004"
+              ]
+            }),
+          }}
+        />
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
 
